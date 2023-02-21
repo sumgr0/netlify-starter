e9ac8d718610b286a66a360ed30f548dc765d324
sumgr0 <sumit.grover@gmail.com>
2023-02-21 17:19:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Subdomain Takeover by sumgr0!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         Twitter: sumgr0
         </p>
       </main>
 
